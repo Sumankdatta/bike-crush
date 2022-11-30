48116e7cf9e03615083f2fdf564e3d13e2968603
Sumankdatta <sumankdatta18@gmail.com>
2022-11-30 20:30:01 +0600

    all seller halmet

--- src/Pages/Dashboard/Allusers/Allusers.js
+++ src/Pages/Dashboard/Allusers/Allusers.js
@@ -1,8 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import useTitle from '../../../hooks/useTitle';
 
 const Allusers = () => {
-
+    useTitle('Allsellers')
     const { data: users = [] } = useQuery({
         queryKey: ['users'],
         queryFn: async() =>{ 
